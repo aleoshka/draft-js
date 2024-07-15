e5fa3eb18276a3e5f980127942f4eebf7fef3a6f
alioshka <macarena7777777@gmail.com>
2024-07-15 09:03:33 +0300

    26

--- app.js
+++ app.js
@@ -1,99 +1,120 @@
-// 24.3
-// Создаем объект items
-let items1 = new Set();
+// 26.2
+let arr1 = new Array("вода", "огонь");
+let arr2 = Array("вода", "огонь");
+let arr3 = Array(2);
 
-// Добавляем новый элемент "дерево" в объект items
-items1.add("дерево");
+console.log(typeof arr1); // object
+console.log(arr1.length); // 2
+console.log(arr1);        // ["вода", "огонь"]
 
-// Добавляем новые элементы "камень", "золото", "серебро" используя цепочку вызовов.
-items1.add("камень").add("золото").add("серебро");
+console.log(typeof arr2); // object
+console.log(arr2.length); // 2
+console.log(arr2);        // ["вода", "огонь"]
 
-// Выводим объект в консоль
-console.log(items1);
+console.log(typeof arr3); // object
+console.log(arr3.length); // 2
+console.log(arr3);        // []
 
-// Set(4) { 'дерево', 'камень', 'золото', 'серебро' }
+// 26.3
+let players = ["Майк", "Дастин", "Лукас", "Макс"];
+let enemies = [];
+let points = [100];
+let active = false;
+let username = null;
+let health = 100;
+let name = "Лукас";
+let score;
 
-// 24.4
-// Создаем объект items
-let items2 = new Set();
+console.log(Array.isArray(players));  // true
+console.log(Array.isArray(enemies));  // true
+console.log(Array.isArray(points));   // true
 
-// Добавляем новые элементы "камень", "золото", "серебро" используя цепочку вызовов.
-items2.add("камень").add("золото").add("серебро");
-
-// Удаляем все элементы из объекта items
-items2.clear();
-
-// Выводим объект в консоль
-console.log(items2);
-
-// Set(0) {}
-
-// 24.5
-// Создаем объект items
-let items3 = new Set();
-
-// Добавляем новые элементы "камень", "золото", "серебро" используя цепочку вызовов.
-items3.add("камень").add("золото").add("серебро");
-
-// Удаляем элементы из объекта items
-console.log(items3.delete("камень")); // true
-console.log(items3.delete("дерево")); // false
-
-// Выводим объект в консоль
-console.log(items3);
-
-// Set(2) { 'золото', 'серебро' }
-
-// 24.6
-let items4 = new Set(["камень", "золото", "серебро"]);
-
-console.log(items4.entries());
-
-for (let entry of items4.entries()) {
-    console.log(entry);
-}
+console.log(Array.isArray(active));   // false
+console.log(Array.isArray(username)); // false
+console.log(Array.isArray(health));   // false
+console.log(Array.isArray(name));     // false
+console.log(Array.isArray(score));    // false
 
 console.clear();
 
-// 24.7
-// Определяем набор игровых сущностей
-let entities = new Set();
+// 26.4
+// Определить все ли числа в массиве четные.
+let numbers = [2, 4, 6, 8, 10];
+let allEven = numbers.every(num => num % 2 === 0);
+console.log(allEven); // true
 
-// Добавляем несколько игровых объектов в набор
-let player = { id: 1, type: 'player' };
-let enemy1 = { id: 2, type: 'enemy' };
-let enemy2 = { id: 3, type: 'enemy' };
+// Проверка того, все ли элементы в массиве больше определенного значения
+const nums = [10, 20, 30, 40, 50];
+const allGreater = nums.every(num => num > 5);
+console.log(allGreater); // true
 
-entities.add(player);
-entities.add(enemy1);
-entities.add(enemy2);
+// Проверка, удовлетворяют ли все элементы массива нескольким условиям
+const arr = [{ name: "Лия", age: 21 }, { name: "Макс", age: 19 }, { name: "Майкл", age: 19 }];
+const allMatch = arr.every(person => person.age > 18 && person.name.length > 2);
+console.log(allMatch); // true
+//
+const numbers1 = [1, 2, 3, 4, 5];
 
-console.log(entities);
+const result = numbers1.every((currentValue, index, array) => {
+    if (index === 0) {
+        return true; // пропускаем первый элемент
+    } else {
+        return currentValue > array[index - 1];
+    }
+});
 
-function updatePosition(entity) {
-    // ... обновить позицию игровой сущности
-    console.log(`Обновленная позиция объекта ${entity.id}`);
-}
+console.log(result); // true
 
-// Вызов функции updatePosition для каждого игрового объекта.
-entities.forEach(updatePosition);
+// 26.5
+// Объединение двух массивов
+
+let items1 = ["Зелье", "Эликсир"];
+let items2 = ["Свиток", "Жезл"];
+let allItems = items1.concat(items2);
+
+console.log(allItems); // ["Зелье", "Эликсир", "Свиток", "Жезл"]
+
+
+// Объединение массива и одного элемента
+
+let items = ["Зелье", "Эликсир"];;
+let allItems1 = items.concat("Свиток");
+
+console.log(allItems); // ["Зелье", "Эликсир", "Свиток"]
+
+// 26.6
+let playerPositions = [
+    { x: 10, y: 20 },
+    { x: 20, y: 30 },
+    { x: 30, y: 40 }
+];
+
+let a = playerPositions.forEach(pos => ({ x: pos.x + 5, y: pos.y + 10 }));
+
+console.log(a);
+//
+let damages = [10, 20, 30];
+let damageMessages = damages.map(damage => `Урон ${damage} очков.`);
+
+console.log(damageMessages);
+
+// 26.7
+let items3 = ["Зелье", "Эликсир", "Свиток", "Жезл"];
+
+let hasScroll = items3.includes("Свиток");
+let hasAmulet = items3.includes("Амулет");
+
+console.log(hasScroll); // true
+console.log(hasAmulet); // false
 
 console.clear();
 
-// 24.8
-let items5 = new Set(["меч", "щит", "шлем"]);
+// 26.8
+let enemies1 = [
+    { name: "Гоблин", health: 10 },
+    { name: "Скелет", health: 20 },
+    { name: "Орк", health: 30 }
+];
 
-console.log(items5.has("меч"));   // true
-console.log(items5.has("посох")); // false
-
-console.clear();
-
-// 24.9
-let items6 = new Set(["меч", "щит", "шлем"]);
-
-console.log(items6.keys()); // [Set Iterator] { 'меч', 'щит', 'шлем' }
-
-// 24.10
-let items7 = new Set(["меч", "щит", "шлем"]);
-
-console.log(items7.values()); // [Set Iterator] { 'меч', 'щит', 'шлем' }
+let lowHealthEnemies = enemies1.filter(enemy => enemy.health < 20);
+console.log(lowHealthEnemies[0]);
