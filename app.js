be7724d84a9dfcf303d34082be0055cf71b2559a
Лёха домашний <macarena7777777@gmail.com>
2024-07-31 18:59:08 +0300

    28.4

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 'use strict';
 
-// Создаем объекты игроков
-let player1 = { name: 'Макс', score: 10 };
-let player2 = { name: 'Лукас', score: 20 };
+let character = {};
 
-let players = Object.assign({ player_i: player1 }, { player1_j: player2 });
-console.log(players);
-console.log(player1);
+Object.defineProperties(character, {
+    name: {
+        value: "Дасьтин";
+        writable: true
+    },
+    active: {
+        value: true,
+        writable: true
+    },
+    health: {
+        value: 100,
+        writable: true
+    },
+    speed: {
+        value: 10,
+        writable: false
+    },
+    weapon: {
+        value: "Бита",
+        writable: true
+    }
+})
+
+console.log(character.name);    // Дастин
+console.log(character.active);  // true
+console.log(character.health);  // 100
+console.log(character.speed);   // 10
+console.log(character.weapon);  // Бита
+
+character.health = 99;
+
+console.log(character.health);  // 99
+
+character.speed = 20;
+
+console.log(character.speed);  // 10
