7bf6b49b701e6fb95fce1ebaeab67482701b5cf3
Лёха домашний <macarena7777777@gmail.com>
2024-07-31 19:47:55 +0300

    28.8

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 'use strict';
 
-let enemy = {
-    name: "Демогоргон",
-    power: 100,
-    location: "Хоукинс"
-};
+let enemy = [
+    ['name', 'Демогоргон'],
+    ['power', 100],
+    ['location', 'Хоукинс']
+];
 
-enemy.power = 90;
-console.log(enemy.power); // 90
-
-Object.freeze(enemy);
-
-enemy.power = 100;
-console.log(enemy.power); // 90
+let enemyObj = Object.fromEntries(enemy);
+console.log(enemyObj);
