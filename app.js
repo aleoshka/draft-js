a91479ddb407b2e46fc563780eec2d193c323e6f
Лёха домашний <macarena7777777@gmail.com>
2024-07-31 19:42:15 +0300

    28.5

--- app.js
+++ app.js
@@ -2,39 +2,9 @@
 
 let character = {};
 
-Object.defineProperties(character, {
-    name: {
-        value: "Дасьтин";
-        writable: true
-    },
-    active: {
-        value: true,
-        writable: true
-    },
-    health: {
-        value: 100,
-        writable: true
-    },
-    speed: {
-        value: 10,
-        writable: false
-    },
-    weapon: {
-        value: "Бита",
-        writable: true
-    }
+Object.defineProperty(character, "name", {
+    value: "Дасьтин Хоффман",
+    writable: true
 })
 
-console.log(character.name);    // Дастин
-console.log(character.active);  // true
-console.log(character.health);  // 100
-console.log(character.speed);   // 10
-console.log(character.weapon);  // Бита
-
-character.health = 99;
-
-console.log(character.health);  // 99
-
-character.speed = 20;
-
-console.log(character.speed);  // 10
+console.log(character);
