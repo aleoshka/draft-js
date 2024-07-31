1a3a8bf89ff9ff5fb6a38d59caeac653e98824f3
Лёха домашний <macarena7777777@gmail.com>
2024-07-31 19:56:21 +0300

    28.14

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ let enemy = {
     location: "Изнанка"
 };
 
-console.log(Object.isSealed(enemy)); // false
+Object.defineProperty(enemy, "health", {
+    value: 9000000,
+    writable: true
+});
 
-Object.seal(enemy); // Запечатываем объект
+console.log(Object.isExtensible(enemy)); // true
 
-console.log(Object.isSealed(enemy)); // true
+Object.preventExtensions(enemy);
+
+enemy.health = 9990000;
+
+console.log(Object.isExtensible(enemy)); // false
+console.log(enemy.health); // 9990000
+
+// TypeError: Cannot define property hive_mind, object is not extensible
+Object.defineProperty(enemy, "hive_mind", {
+    value: true,
+    writable: true
+});
