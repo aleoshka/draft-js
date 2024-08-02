90f0c1f84b1b7f2ed220794392b840f20fc33af7
Лёха домашний <macarena7777777@gmail.com>
2024-08-02 22:42:37 +0300

    30.10

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 'use strict';
 
-let email = "lia@example.com";
+let str = "Привет, Лия";
 
-//Пример проверки валидности емейла:
-if (email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-    console.log("Валидный емейл");
-} else {
-    console.log("Невалидный емейл");
-}
-
-//Пример проверки валидности кредитной карточки:
-let creditCardNumber = "1234-5678-0123-1234";
-
-if (creditCardNumber.match(/^\d{4}-\d{4}-\d{4}-\d{4}$/)) {
-    console.log("Валидный номер");
-} else {
-    console.log("Невалидный номер");
-}
+console.log(str.padEnd(30, "!"));
