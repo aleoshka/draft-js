acbd209fc5fb4b41c38dbb07be3f9ed59d94bba2
Лёха домашний <macarena7777777@gmail.com>
2024-08-02 22:12:35 +0300

    30.5

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
 
-let str = "Привет";
-console.log(str.concat(", ", "Лия", "!")); // Привет, Лия!
+let str = "Этот метод является регистрозависимым.";
+
+console.log(str.endsWith("."));     // true
+console.log(str.endsWith("!"));     // false
+console.log(str.endsWith("д", 10)); // true
