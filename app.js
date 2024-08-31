cd4bb40c6fa45f37b4761b60087a9a7697e8f58e
Лёха домашний <macarena7777777@gmail.com>
2024-08-31 19:26:44 +0300

    34.3

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
 'use strict';
 
-// Основание натурального логарифма, e
-// 2.718281828459045
-console.log(Math.E);
-
-// Натуральный логарифм из 10
-// 2.302585092994046
-console.log(Math.LN10);
-
-// Натуральный логарифм из 2
-// 0.6931471805599453
-console.log(Math.LN2);
-
-// Десятичный логарифм из e
-// 0.4342944819032518
-console.log(Math.LOG10E);
-
-// Двоичный логарифм из e
-// 1.4426950408889634
-console.log(Math.LOG2E);
-
-// Отношение длины окружности круга к его диаметру
-// 3.141592653589793
-console.log(Math.PI);
-
-// Квадратный корень из 1/2
-// 0.7071067811865476
-console.log(Math.SQRT1_2);
-
-// Квадратный корень из 2
-// 1.4142135623730951
-console.log(Math.SQRT2);
+console.log(Math.abs('-1'));     // 1
+console.log(Math.abs(-2));       // 2
+console.log(Math.abs(null));     // 0
+console.log(Math.abs(''));       // 0
+console.log(Math.abs([]));       // 0
+console.log(Math.abs([2]));      // 2
+console.log(Math.abs([1, 2]));    // NaN
+console.log(Math.abs({}));       // NaN
+console.log(Math.abs('string')); // NaN
+console.log(Math.abs());         // NaN
