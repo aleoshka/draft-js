43e14638613b37eff13b0983dbae2f6484624797
Лёха домашний <macarena7777777@gmail.com>
2024-08-31 19:30:36 +0300

    34.7

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-console.log(Math.max(10, 20));   //  20
-console.log(Math.max(-10, -20)); // -10
-console.log(Math.max(-10, 20));  //  20
+console.log(Math.min(10, 20));   //  10
+console.log(Math.min(-10, -20)); // -20
+console.log(Math.min(-10, 20));  // -10
 
 let data = [5, 2, 3, 4];
-console.log(Math.max(...data)); // 5
+console.log(Math.min(...data)); // 2
