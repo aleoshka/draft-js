a2fb3ee596837d0b6fa5b29974d3e3f1d60277f6
Лёха домашний <macarena7777777@gmail.com>
2024-08-31 19:33:55 +0300

    34.11

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
-console.log(Math.round(42.42));   // 42
-console.log(Math.round(42.52));   // 43
-console.log(Math.round(- 42.42)); // -42
+console.log(Math.sqrt(4));  // 2
+console.log(Math.sqrt(3));  // 1.7320508075688772
+console.log(Math.sqrt(2));  // 1.4142135623730951
+console.log(Math.sqrt(1));  // 1
+console.log(Math.sqrt(0));  // 0
+console.log(Math.sqrt(-1)); // NaN
