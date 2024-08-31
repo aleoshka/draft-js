57077143c05e396d171db6e6bfe9bc672c3323cf
Лёха домашний <macarena7777777@gmail.com>
2024-08-31 19:30:00 +0300

    34.6

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 'use strict';
 
-console.log(Math.ceil(42.42));  //  43
-console.log(Math.ceil(-42.42)); // -42
+console.log(Math.max(10, 20));   //  20
+console.log(Math.max(-10, -20)); // -10
+console.log(Math.max(-10, 20));  //  20
+
+let data = [5, 2, 3, 4];
+console.log(Math.max(...data)); // 5
