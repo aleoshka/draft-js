e0660bf720c3afd056341800fc07f87935e33e2d
Лёха домашний <macarena7777777@gmail.com>
2024-08-31 19:27:52 +0300

    34.4

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 'use strict';
 
-console.log(Math.abs('-1'));     // 1
-console.log(Math.abs(-2));       // 2
-console.log(Math.abs(null));     // 0
-console.log(Math.abs(''));       // 0
-console.log(Math.abs([]));       // 0
-console.log(Math.abs([2]));      // 2
-console.log(Math.abs([1, 2]));    // NaN
-console.log(Math.abs({}));       // NaN
-console.log(Math.abs('string')); // NaN
-console.log(Math.abs());         // NaN
+console.log(Math.floor(42.42));  //  42
+console.log(Math.floor(-42.42)); // -43
